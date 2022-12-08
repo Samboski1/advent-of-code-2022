d88a3bacd529f5086fd52bba0fe24edb431a45cc
Alex Muller <alex@mullr.net>
2022-12-08 08:36:07 +0000

    Day 6 part 2

--- day-06/main.ts
+++ day-06/main.ts
@@ -1,12 +1,12 @@
-export const partOne = (values: string[]) => {
-    let character = 3
+const findStreams = (values: string[], character: number) => {
     let repeatedCharacters = true
+    let initialCharacter = character
 
     const dataStream = values[0].split('')
 
     while (repeatedCharacters == true) {
         character += 1
-        let streamSubset = dataStream.slice(character-4, character)
+        let streamSubset = dataStream.slice(character-(initialCharacter+1), character)
         let deduplicatedStreamLength = [...new Set(streamSubset)].length
         if (streamSubset.length == deduplicatedStreamLength) {
             repeatedCharacters = false
@@ -16,5 +16,10 @@ export const partOne = (values: string[]) => {
     return character
 }
 
+export const partOne = (values: string[]) => {
+    return findStreams(values, 3)
+}
+
 export const partTwo = (values: string[]) => {
+    return findStreams(values, 13)
 }
